737b78558543fcfad7b39db688310bf4d5d6729c
Admin <Admin@DESKTOP-HHTUPPP>
2019-06-15 17:50:33 +0800

    SpringCloud微服务案例-部门服务消费者

---
+++ microservicecloud-consumer-dept-80/src/main/java/com/wez/springcloud/DeptConsumer80_APP.java
@@ -0,0 +1,13 @@
+package com.wez.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptConsumer80_APP {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeptConsumer80_APP.class, args);
+	}
+	
+}
---
+++ microservicecloud-provider-dept-8001/src/main/java/com/wez/springcloud/DeptProvider8001_APP.java
@@ -0,0 +1,13 @@
+package com.wez.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptProvider8001_APP {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeptProvider8001_APP.class, args);
+	}
+	
+}
