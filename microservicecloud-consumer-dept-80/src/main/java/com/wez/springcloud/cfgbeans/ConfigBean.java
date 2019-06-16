d8a8dce6027a12239da0c469a94630b31ccee627
Admin <Admin@DESKTOP-HHTUPPP>
2019-06-16 18:28:19 +0800

    设置Ribbon负载均衡策略

--- microservicecloud-consumer-dept-80/src/main/java/com/wez/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/wez/springcloud/cfgbeans/ConfigBean.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+
 @Configuration
 public class ConfigBean {
 
@@ -12,6 +16,13 @@ public class ConfigBean {
 	@LoadBalanced // Ribbon负载均衡，默认是轮询
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+	
+	// 设置轮询策略。（使用Ribbon提供的负载均衡策略）
+	@Bean
+	public IRule loadRalancedRule() {
+//		return new RoundRobinRule(); // 轮询策略，默认的。
+		return new RandomRule(); // 随机策略
 	}
 	
 }
