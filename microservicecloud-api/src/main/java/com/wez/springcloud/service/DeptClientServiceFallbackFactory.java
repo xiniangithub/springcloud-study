3a4a892d9f991880daa639c78eb0cab62e39602f
Admin <Admin@DESKTOP-HHTUPPP>
2019-06-22 11:50:47 +0800

    SpringCloud Hystrix服务降级处理

---
+++ microservicecloud-api/src/main/java/com/wez/springcloud/service/DeptClientServiceFallbackFactory.java
@@ -0,0 +1,40 @@
+package com.wez.springcloud.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.wez.springcloud.entities.Dept;
+
+import feign.hystrix.FallbackFactory;
+
+@Component
+public class DeptClientServiceFallbackFactory implements FallbackFactory<DeptClientService> {
+
+	@Override
+	public DeptClientService create(Throwable cause) {
+		
+		System.out.println("--------------------");
+		cause.printStackTrace();
+		
+		return new DeptClientService() {
+
+			@Override
+			public Dept get(long id) {
+				return new Dept().setDeptno(id).setDname("该ID：" + id + "没有没有对应的信息,Consumer客户端提供的降级信息,此刻服务Provider已经关闭")
+						.setDb_source("no this database in MySQL");
+			}
+
+			@Override
+			public List<Dept> list() {
+				return null;
+			}
+
+			@Override
+			public boolean add(Dept dept) {
+				return false;
+			}
+
+		};
+	}
+}
